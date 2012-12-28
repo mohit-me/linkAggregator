d7994176d5b01d2ec091ae81d7310ff63e4d5328
Kamal Joshi <k2joshi04@gmail.com>
2012-12-28 19:43:12 +0530

    'init'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkAggregator::Application.config.secret_token = 'a53e69b28c27dfe249e6acc4bf30d194fc5bfddbb5d0348bbc92c6815a8590fcf8e881b05a7396954807e6c759ee25d08b04910100ee39927935e43bfeb00bd6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LinkAggregator::Application.config.session_store :cookie_store, key: '_linkAggregator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LinkAggregator::Application.config.session_store :active_record_store
